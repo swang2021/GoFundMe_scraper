67370690771fc9c6929a353f386ad257cee0f190
swang2021 <48528385+WallStreetJournal@users.noreply.github.com>
2021-01-24 17:47:26 -0500

    Update GoFundMe_scraper.py

--- GoFundMe_scraper.py
+++ GoFundMe_scraper.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 __author__ =  'S. Wang'
